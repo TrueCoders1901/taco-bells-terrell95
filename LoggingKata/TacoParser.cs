f912e7cf961f8f461d4587df6024f99c97a66cc2
terrell oliver <mroliver1995@aol.com>
2019-03-01 14:56:50 -0600

    Added Parse method

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -11,8 +11,39 @@
         {
             logger.LogInfo("Begin parsing");
 
-            // Do not fail if one record parsing fails, return null
-            return null; // TODO Implement
+             // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+             string[] cells = line.Split(',');
+
+            // If your array.Length is less than 3, something went wrong
+            if (cells.Length < 3)
+            {
+                // Log that and return null
+                return null;
+            }
+
+            // grab the latitude from your array at index 0
+            double latitude = double.Parse(cells[0]);
+            // grab the longitude from your array at index 1
+            double longitude = double.Parse(cells[1]);
+            // grab the name from your array at index 2
+            string name = cells[2];
+
+            // You'll need to create a TacoBell class
+            // that conforms to ITrackable
+
+            // Then, you'll need an instance of the TacoBell class
+            TacoBell taco = new TacoBell();
+            taco.Name = name;
+
+            Point p = new Point();
+            p.Longitude = longitude;
+            p.Latitude = latitude;
+            // With the name and point set correctly
+            taco.Location = p;
+
+            // Then, return the instance of your TacoBell class
+            // Since it conforms to ITrackable
+            return taco;
         }
     }
 }
